ba1c0e4372782170c61f3e1c241960cf445e2ce2
mk10969 <mk10969@gmail.com>
2020-04-13 23:26:13 +0900

    バグfix

--- websocket-server/src/main/java/com/example/websocket/server/configuration/QuoteProperties.java
+++ websocket-server/src/main/java/com/example/websocket/server/configuration/QuoteProperties.java
@@ -1,24 +1,26 @@
 package com.example.websocket.server.configuration;
 
 import com.example.websocket.server.model.Quote;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class QuoteProperties {
 
-    private static final String path = "classpath:quotes.json";
-
-    private final ResourceLoader resourceLoader;
+    @Value("classpath:quotes.json")
+    private Resource resourceFile;
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -27,20 +29,28 @@ public class QuoteProperties {
 
     @Bean
     public List<Quote> quotes(ObjectMapper objectMapper) {
-        try {
-            return objectMapper.readValue(readJsonFile(), new TypeReference<>() {
-            });
-        } catch (IOException e) {
-            throw new RuntimeException("parse error", e);
-        }
-    }
+        // AutoClosable
+        try (InputStream inputStream = resourceFile.getInputStream()) {
 
-    private File readJsonFile() {
-        try {
-            return resourceLoader.getResource(path).getFile();
+            return objectMapper.readValue(inputStream, new TypeReference<>() {
+            });
+
+        } catch (JsonParseException | JsonMappingException e) {
+            throw new IllegalArgumentException("Jsonのパースに失敗しました。", e);
+
         } catch (IOException e) {
             throw new IllegalStateException("ファイルの読み込みに失敗しました。", e);
         }
     }
+
+//    private File readJsonFile() {
+//        try {
+//            // File Systemとして読み込めないみたい。
+//            // https://stackoverflow.com/questions/14876836/file-inside-jar-is-not-visible-for-spring
+//            return resourceFile.getFile();
+//        } catch (IOException e) {
+//            throw new IllegalStateException("ファイルの読み込みに失敗しました。", e);
+//        }
+//    }
 
 }
